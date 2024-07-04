718d15a3a5d5d71e23f40c4e842f97a7062adef8
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2024-07-04 14:57:01 +0300

    packages added

--- DevSeek/obj/Debug/net8.0/DevSeek.AssemblyInfo.cs
+++ DevSeek/obj/Debug/net8.0/DevSeek.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevSeek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47dad5742afd6437099fa58c91de3a393cffdd11")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevSeek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevSeek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
