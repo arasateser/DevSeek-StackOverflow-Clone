bbafa75f550f27b3391518078ef7780427ce04df
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2024-07-17 14:51:48 +0300

    comment model updated

--- DevSeek/Models/Comment.cs
+++ DevSeek/Models/Comment.cs
@@ -3,9 +3,10 @@ namespace DevSeek.Models
     public class Comment
     {
         public int Id { get; set; }
-        public string Body { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public ApplicationUser UserId { get; set; }
-        public Question QuestionId { get; set; }
+        public string Content { get; set; }
+        public User Author { get; set; }
+        public Question Question { get; set; }
+        public List<Vote> Votes { get; set; }
     }
+
 }
